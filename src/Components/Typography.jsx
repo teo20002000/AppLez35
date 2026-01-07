c9d9a15d6addb1153df04e637af0fe1f6e64ee7b
Matteo Renigaldo <teor1805@gmail.com>
2026-01-07 14:03:33 +0100

    aggiunto Typography

---
+++ src/Components/Typography.jsx
@@ -0,0 +1,12 @@
+import "./Typography.css"
+
+function Typography({children, title}) {
+    if (title) {
+        return <span className="title">{children}</span>
+    }
+    return <span>{children}</span>
+    
+
+}
+
+export default Typography
