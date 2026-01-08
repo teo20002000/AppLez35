d2b1757bdbad2f6da7b53f82d1f5ccfce08abff1
Matteo Renigaldo <teor1805@gmail.com>
2026-01-08 17:43:45 +0100

    Aggiornato Typography e aggiungo Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Typography from './Components/Typography.jsx';
+import Card from './Components/Card.jsx'
 import './App.css';
+
 
 function App() {
   return ( <>
   <Typography>Typography : normal</Typography> <br />
-  <Typography title={true}>Typography : title</Typography>
+  <Typography color={{color:"#519C11"}}>Typography color</Typography> <br />
+  <Typography componentType="h1">Typography : title</Typography> <br />
+  <Card title="card-title" image="./SimpleCode.jpg">Card</Card>
   </>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,21 @@
+import "./Card.css"
+import Typography from "./Typography";
+
+function Card({children,image,title}) {
+    return (
+    <div className="card">
+        <img src={image} className="img"/>
+        <div className="card-body">
+        <Typography componentType="h3" color={{color:"white"}}>
+            {title}
+        </Typography>
+        <Typography componentType="paragraph" color={{color:"white"}}>
+            {children}
+        </Typography>
+        </div>
+    </div>
+
+    )
+}
+
+export default Card;
